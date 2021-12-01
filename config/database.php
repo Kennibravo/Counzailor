5d041a0e59c250554358085e0586d7e6a93a28e5
Kehinde <kehinde.alade@patricia.com.ng>
2021-12-01 15:16:35 +0100

    Added database config

---
+++ config/database.php
@@ -0,0 +1,34 @@
+<?php
+// mysql://b1d8eb5ef5c949:ef2666a5@us-cdbr-east-04.cleardb.com/heroku_9311dc1e385daf4?reconnect=true
+return [
+
+   'default' => 'accounts',
+
+   'connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'us-cdbr-east-04.cleardb.com'),
+            'port'      => env('DB_PORT', 3306),
+            'database'  => env('DB_DATABASE', 'heroku_9311dc1e385daf4'),
+            'username'  => env('DB_USERNAME', 'b1d8eb5ef5c949'),
+            'password'  => env('DB_PASSWORD', 'ef2666a5'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+         ],
+
+        'mysql2' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB2_HOST'),
+            'port'      => env('DB_PORT'),
+            'database'  => env('DB2_DATABASE'),
+            'username'  => env('DB2_USERNAME'),
+            'password'  => env('DB2_PASSWORD'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+    ],
+];
